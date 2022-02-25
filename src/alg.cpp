db777be4a88ac41a69e7746e04f768ca9388a015
Denis Suchkov <99133137+PlagueDoctoh@users.noreply.github.com>
2022-02-25 13:53:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-  for (int i = 2; i <= (value/2);i++) {
+  for (int i = 2; i <= (value/2); i++) {
     if ((value%i) == 0)
       return false;
   }
@@ -15,8 +15,8 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
   int n_Prime = 0;
-  while(n != 0) {
-    for (int i = 0;i <= n;i++) {
+  while (n != 0) {
+    for (int i = 0; i <= n; i++) {
       n_Prime++;
       if (checkPrime(n_Prime) == true) {
         n--;
@@ -39,7 +39,7 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
   uint64_t sum = 0;
-  for (uint64_t i = 1;i<hbound;i++) {
+  for (uint64_t i = 1; i < hbound; i++) {
     if checkPrime(i) == true
       sum+=i;
   }
