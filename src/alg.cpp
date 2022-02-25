c6736a72185cc208ead27eda5aea39d39485253c
Denis Suchkov <99133137+PlagueDoctoh@users.noreply.github.com>
2022-02-25 17:06:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,7 @@
 bool checkPrime(uint64_t value) {
 // вставьте код функции
   for (int i = 2; i <= (value/2); i++) {
-    if ((value%i) == 0 or value <= 1)
+    if ((value%i) == 0 || value <= 1)
       return false;
   }
   return true;
